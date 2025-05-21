c663eab2fd02972d18af04b84db02cc7ce37d535
luis_flv@outlook.com <luis_flv@outlook.com>
2025-05-21 14:57:22 -0300

    Salvando o projeto Vetor

---
+++ Vetor/VetorExemplo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorExemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetNomes = new string[3];
+
+            //vetNomes[0] = "Juquinha";
+            //vetNomes[1] = "Maria";
+            //vetNomes[2] = "Teobaldo";
+
+            Console.WriteLine("Informe o primeiro nome: ");
+            vetNomes[0] = Console.ReadLine();
+
+            Console.WriteLine("Informe o segundo nome: ");
+            vetNomes[1] = Console.ReadLine();
+
+            Console.WriteLine("Informe o terceiro valor: ");
+            vetNomes[2] = Console.ReadLine();
+
+            Console.WriteLine("===APRESENTANDO OS VALORES DO VETOR===");
+
+            Console.WriteLine(vetNomes[0]);
+            Console.WriteLine(vetNomes[1]);
+            Console.WriteLine(vetNomes[2]);
+
+        }
+    }
+}
