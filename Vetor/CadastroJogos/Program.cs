3f0b67217404a65c86175f439c82ca6000aaa986
luis_flv@outlook.com <luis_flv@outlook.com>
2025-05-22 09:17:41 -0300

    Criação do projeto Cadastro Jogos

---
+++ Vetor/CadastroJogos/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroJogos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARAR O VETOR DE NOMES DE JOGOS
+            string[] vetJogos = new string[3];
+
+            do
+            {
+
+
+                //MENU
+                Console.WriteLine(@"
+░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░  ██████╗░███████╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗  ██╔══██╗██╔════╝
+██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║  ██║░░██║█████╗░░
+██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║  ██║░░██║██╔══╝░░
+╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝  ██████╔╝███████╗
+░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ╚═════╝░╚══════╝
+
+░░░░░██╗░█████╗░░██████╗░░█████╗░░██████╗
+░░░░░██║██╔══██╗██╔════╝░██╔══██╗██╔════╝
+░░░░░██║██║░░██║██║░░██╗░██║░░██║╚█████╗░
+██╗░░██║██║░░██║██║░░╚██╗██║░░██║░╚═══██╗
+╚█████╔╝╚█████╔╝╚██████╔╝╚█████╔╝██████╔╝
+░╚════╝░░╚════╝░░╚═════╝░░╚════╝░╚═════╝░");
+
+                Console.WriteLine("1 - Cadastrar");
+                Console.WriteLine("2 - Consultar");
+                Console.WriteLine("3 - Atualizar");
+                Console.WriteLine("4 - Excluir");
+                int opcao = int.Parse(Console.ReadLine());
+
+                if (opcao == 1)
+                {
+                    //CADASTRAR NOME NO VETOR
+                    for (int i = 0; i < vetJogos.Length; i++)
+                    {
+                        Console.WriteLine($"Informe o {i + 1}º nome do jogo: ");
+                        vetJogos[i] = Console.ReadLine();
+                    }
+
+                    Console.WriteLine("Para continuar, pressione a tecla Enter");
+                    Console.ReadKey();
+
+                }
+                else if (opcao == 2)
+                {
+                    //CONSULTAR OS VALORES DO VETOR
+                    for (int i = 0; i < vetJogos.Length; i++)
+                    {
+                        Console.WriteLine($"Posição: {i} | Nome do jogo: {vetJogos[i]}");
+                    }
+
+                    Console.WriteLine("Para continuar, pressione a tecla Enter");
+                    Console.ReadKey();
+                }
+                else if (opcao == 3)
+                {
+                    //IMPLEMENTAR A FUNÇÃO DE ATUALIZAR
+                    //SOLICITAR O ÍNDICE E O NOVO NOME
+                    //ALTERAR O NOME NO VETOR
+                }
+                else if (opcao == 4)
+                {
+                    //IMPLEMENTAR A FUNÇÃO DE EXCLUIR
+                    //SOLICITAR O ÍNDICE DO NOME DO JOGO PARA O USUÁRIO
+                    //ATRIBUIR UM VALOR " " PARA EXCLUIR
+                }
+                Console.Clear();
+
+            } while (true);
+
+        }
+    }
+}
